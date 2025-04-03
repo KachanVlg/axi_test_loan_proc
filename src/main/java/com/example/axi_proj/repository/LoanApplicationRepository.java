60812e482fa3dfad20460601a42947ba763a225f
KachanVlg <vlad.bend@inbox.ru>
2025-04-03 16:31:40 +0300

    feat: searching list of approved applications created

--- src/main/java/com/example/axi_proj/repository/LoanApplicationRepository.java
+++ src/main/java/com/example/axi_proj/repository/LoanApplicationRepository.java
@@ -2,8 +2,14 @@ package com.example.axi_proj.repository;
 
 
 import com.example.axi_proj.domain.model.loanApplication.LoanApplication;
+import com.example.axi_proj.domain.model.loanApplication.LoanApplicationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long> {}
+public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long> {
+
+    List<LoanApplication> findLoanApplicationsByStatus(LoanApplicationStatus status);
+}
--- src/main/java/com/example/axi_proj/service/LoanApplicationServiceImpl.java
+++ src/main/java/com/example/axi_proj/service/LoanApplicationServiceImpl.java
@@ -2,12 +2,21 @@ package com.example.axi_proj.service;
 
 
 import com.example.axi_proj.domain.model.loanApplication.LoanApplication;
+import com.example.axi_proj.domain.model.loanApplication.LoanApplicationStatus;
+import com.example.axi_proj.repository.LoanApplicationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class LoanApplicationServiceImpl implements LoanApplicationService{
+
+    private final LoanApplicationRepository repository;
+
     @Override
     public LoanApplication save(LoanApplication application) {
         return null;
@@ -15,6 +24,6 @@ public class LoanApplicationServiceImpl implements LoanApplicationService{
 
     @Override
     public List<LoanApplication> listApproved(int page, int pageSize) {
-        return List.of();
+        return repository.findLoanApplicationsByStatus(LoanApplicationStatus.APPROVED);
     }
 }
