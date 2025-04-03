f1aa7b144bce3ff00c94d8ab81217626865fb26e
KachanVlg <vlad.bend@inbox.ru>
2025-04-03 16:39:40 +0300

    refactor: added pageable search to LoanAgreementRepository

--- src/main/java/com/example/axi_proj/repository/LoanApplicationRepository.java
+++ src/main/java/com/example/axi_proj/repository/LoanApplicationRepository.java
@@ -3,6 +3,7 @@ package com.example.axi_proj.repository;
 
 import com.example.axi_proj.domain.model.loanApplication.LoanApplication;
 import com.example.axi_proj.domain.model.loanApplication.LoanApplicationStatus;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,5 @@ import java.util.List;
 @Repository
 public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long> {
 
-    List<LoanApplication> findLoanApplicationsByStatus(LoanApplicationStatus status);
+    List<LoanApplication> findLoanApplicationsByStatus(LoanApplicationStatus status, PageRequest pageRequest);
 }
--- src/main/java/com/example/axi_proj/service/LoanApplicationServiceImpl.java
+++ src/main/java/com/example/axi_proj/service/LoanApplicationServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.axi_proj.domain.model.loanApplication.LoanApplication;
 import com.example.axi_proj.domain.model.loanApplication.LoanApplicationStatus;
 import com.example.axi_proj.repository.LoanApplicationRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +26,6 @@ public class LoanApplicationServiceImpl implements LoanApplicationService{
 
     @Override
     public List<LoanApplication> listApproved(int page, int pageSize) {
-        return repository.findLoanApplicationsByStatus(LoanApplicationStatus.APPROVED);
+        return repository.findLoanApplicationsByStatus(LoanApplicationStatus.APPROVED, PageRequest.of(page, pageSize));
     }
 }
